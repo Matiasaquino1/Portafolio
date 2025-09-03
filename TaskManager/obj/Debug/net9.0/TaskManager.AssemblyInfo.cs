a73e313724c08cc2f71cab440a3d361581ae2083
Matiasaquino1 <aquinito2092@gmail.com>
2025-09-02 21:09:50 -0300

    version

--- TaskManager/obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
+++ TaskManager/obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bf542162a2b1e7f78bfafde77de6f9fa96e80e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da97a6756665bcf675d64f28b6accb9157aa00e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
