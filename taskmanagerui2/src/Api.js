a5893805c3fac97bcd36bd8c5307827aa41eaa03
Matiasaquino1 <aquinito2092@gmail.com>
2025-09-16 04:55:28 -0300

    modified axios

--- taskmanagerui2/src/Api.js
+++ taskmanagerui2/src/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://taskmanager-api.onrender.com/api/tasks' 
+  baseURL: 'https://taskmanager-yw4y.onrender.com/api'
 });
 
 export default api;
