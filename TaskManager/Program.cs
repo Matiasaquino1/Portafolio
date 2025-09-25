4c60de562e16883a7804c9736dbad40afa52437e
Matiasaquino1 <aquinito2092@gmail.com>
2025-09-25 20:08:14 -0300

    cors

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -24,21 +24,22 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowGithubPages",
+    options.AddPolicy("AllowAll",
         policy =>
         {
-            policy.WithOrigins("https://matiasaquino1.github.io") 
+            policy.AllowAnyOrigin()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
 });
+
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
-app.UseCors("AllowGithubPages");
+app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
 
